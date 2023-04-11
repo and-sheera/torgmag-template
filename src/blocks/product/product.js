14015df3a3c1c3003d6eb1222c46ad90c2d5e78f
and-sheera <shirnin.andrew@gmail.com>
2023-04-11 22:11:19 +0300

    minor upd

--- src/blocks/product/product.js
+++ src/blocks/product/product.js
@@ -6,16 +6,6 @@ export default function product() {
     hideScrollbar: false,
     Toolbar: {
       display: {
-        // left: ["infobar"],
-        // middle: [
-        //   "zoomIn",
-        //   "zoomOut",
-        //   "toggle1to1",
-        //   "rotateCCW",
-        //   "rotateCW",
-        //   "flipX",
-        //   "flipY",
-        // ],
         right: ['close']
       }
     }
