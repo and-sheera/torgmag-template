f49cd11852b3c222dfb0a70b8167b309b0db82f8
and-sheera <shirnin.andrew@gmail.com>
2023-05-04 21:19:44 +0300

    minor upd

--- src/blocks/_ui/ui-input/ui-input.js
+++ src/blocks/_ui/ui-input/ui-input.js
@@ -5,18 +5,21 @@ import 'parsleyjs/dist/i18n/ru'
 
 export default function uiInput() {
   inputMask()
+  window.inputMask = inputMask
   validation()
 }
 
 function inputMask() {
   for (const input of document.querySelectorAll('[data-mask=tel]')) {
-    IMask(input, {
-      mask: '+{7} (000) 000-00-00'
-    })
+    if (!input.mask) {
+      input.mask = IMask(input, {
+        mask: '+{7} (000) 000-00-00'
+      })
+    }
   }
   const telInput = document.querySelector('input[name="ORDER_PROP_3"]')
-  if (telInput) {
-    IMask(telInput, {
+  if (telInput && !telInput.mask) {
+    telInput.mask = IMask(telInput, {
       mask: '+{7} (000) 000-00-00'
     })
   }
